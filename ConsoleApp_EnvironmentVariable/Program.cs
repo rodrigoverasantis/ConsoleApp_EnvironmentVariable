da881b165b34fa33ab0f997140d6e2607bd42a67
rverasantis <rodrigoverasantis@gmail.com>
2019-05-04 16:09:49 -0400

    Agregar archivos de proyecto.

---
+++ ConsoleApp_EnvironmentVariable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+
+namespace ConsoleApp_EnvironmentVariable {
+  class Program {
+    static void Main(string[] args) {
+
+      var variableName = "password";
+      var variableValue = "abcd1234";
+
+      var environmentVariable = Environment.GetEnvironmentVariable(variableName);
+      if (environmentVariable == null) {
+        Environment.SetEnvironmentVariable(variableName,variableValue,EnvironmentVariableTarget.User);
+      }
+
+      var variables = Environment.GetEnvironmentVariables(EnvironmentVariableTarget.User);
+
+      foreach (DictionaryEntry item in variables) {
+        Console.WriteLine($"Key: {item.Key} - Value: {item.Value}");
+      }
+
+      Console.Read();
+    }
+  }
+}
